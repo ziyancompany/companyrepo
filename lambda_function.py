ab15489d9843b09ee1aa3fbba569ebcc5f2c0711
Martin Yan <48026262+zyan8808@users.noreply.github.com>
2024-04-30 14:48:18 -0700

    Update lambda_function.py
    Added code for read permission grant for all team members when Org base permission is No Permission

--- lambda_function.py
+++ lambda_function.py
@@ -17,3 +17,15 @@ def lambda_handler(event, context):
             branch = repo.get_branch("master")
             branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
             print("Edited the branch protection rules for: " + repo.name)
+
+# function for grant team member read permission on all repos when Org base permission is set to No Permission
+# used for dedicated read permission team like auditing team or security team
+            
+# def lambda_handler(event, context):
+#     org = g.get_organization("your_org_name")
+#     team = org.get_team_by_slug(team_slug)
+#     git_event = event['headers']['x-github-event']
+#     if git_event == "repository":
+#         for repo in org.get_repos():
+#             for member in team.get_members():
+#                 repo.add_to_collaborators(member, permission="pull")
